b44b5b7881bea71d604b4d037408468adb9214d0
“jothammicheni” <jothammurimi2001@gmail.com>
2024-10-16 16:46:15 +0300

    authentication

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,20 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import userRoutes from '../routes/userRoutes'; // Adjust the path to where your userRoutes file is located
+import cookieParser from 'cookie-parser'; // Import cookie-parser
 
 
 dotenv.config();
 
 const app: Express = express();
-app.use(express.json());
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+app.use(cookieParser());
 
-const PORT = process.env.PORT || 3005;  
+// Use the user routes with a specific prefix
+app.use('/api/users', userRoutes);
 
-
-
-
+const PORT = process.env.PORT || 3005;
 
 // Start the server
 app.listen(PORT, () => {
