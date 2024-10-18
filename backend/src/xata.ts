60bf3f12833a04fd49f178dd080b280e6fc47464
“jothammicheni” <jothammurimi2001@gmail.com>
2024-10-18 19:29:06 +0300

    update on xata.ts

--- backend/src/xata.ts
+++ backend/src/xata.ts
@@ -314,35 +314,7 @@ const tables = [
       },
     ],
   },
-=======
-  BaseSchema,
-} from  "@xata.io/client";
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-// Define tables 
-const tables: readonly BaseSchema[] = [
-  {
-    name: 'users',
-    columns: [
-      { name: 'userId', type: 'number '},
-      { name: 'email', type: 'string' },
-      { name: 'password', type: 'string' },
-      { name: 'name', type: 'string' },
-      { name: 'role', type: 'string'} 
-
-    ],
-  },
-  {
-    name: 'products',
-    columns: [
-      // Define product columns here
-      { name: 'productName', type: 'string' },
-      { name: 'price', type: 'number' },
-      // Add more product-specific fields as needed
-    ]
-  }
 
 ] as const;
 
@@ -371,27 +343,10 @@ export type DatabaseSchema = {
   Tasks: TasksRecord;
   Teams: TeamsRecord;
   Users: UsersRecord;
-=======
-// Define the database schema
-export type UserRecord = XataRecord & {
-  userId:number;
-  email: string;
-  password: string;
-  name: string;
-  role:string;
-};
+}
 
-export type ProductRecord = XataRecord & {
-  productName: string;
-  price: number;
-  // Add more fields as necessary
-};
 
-export type DatabaseSchema = {
-  users: UserRecord;
-  products: ProductRecord; // Ensure products are included in the schema
 
-};
 
 const DatabaseClient = buildClient();
 
